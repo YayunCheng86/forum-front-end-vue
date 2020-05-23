17f9864249b2d115da21c36cd073041e4124a59e
Laura <lolacheng86@gmail.com>
2020-05-24 07:34:24 +0800

    Before A16

---
+++ src/components/CreateComment.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent.stop="handleSubmit">
+    <div class="form-group mb-4">
+      <label for="text">留下評論：</label>
+      <textarea v-model="text" class="form-control" rows="3" name="text" />
+    </div>
+    <div class="d-flex align-items-center justify-content-between">
+      <button type="button" class="btn btn-link" @click="$router.back()">回上一頁</button>
+
+      <button type="submit" class="btn btn-primary mr-0">Submit</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import uuid from 'uuid/dist/v4'
+
+export default {
+  props: {
+    restaurantId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log("submit");
+      // TODO: 向 API 發送 POST 請求
+      // 伺服器新增 Comment 成功後...
+      this.$emit('after-create-comment', {
+        commentId: uuid(), // 尚未串接 API 暫時使用隨機的 id
+        restaurantId: this.restaurantId,
+        text: this.text
+      })
+      this.text = ''
+    }
+  }
+};
+</script>>
